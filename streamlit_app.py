84ffea55cf5e1be568a1ce64d1098a75abb9917e
Soujatya Das <soujatya548@gmail.com>
2024-10-06 08:08:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,21 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+import os
 
 uploaded_files = st.file_uploader("Upload a file", type=["pdf"], accept_multiple_files=True)
 
 if uploaded_files:
     all_documents = []
 
+    # Create a directory to store uploaded files temporarily
+    temp_dir = "uploaded_files"
+    os.makedirs(temp_dir, exist_ok=True)
+
     # Iterate over each uploaded file
     for uploaded_file in uploaded_files:
         # Save each file temporarily
-        with open(uploaded_file.name, "wb") as f:
+        with open(os.path.join(temp_dir, uploaded_file.name), "wb") as f:
             f.write(uploaded_file.getbuffer())
         
         # Success message
@@ -43,7 +48,7 @@ def vector_embedding():
     if "vectors" not in st.session_state:
 
         st.session_state.embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-        st.session_state.loader=PyPDFDirectoryLoader("uploaded_files")
+        st.session_state.loader=PyPDFDirectoryLoader("temp_dir")
         st.session_state.docs=st.session_state.loader.load()
         st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs)
@@ -52,7 +57,7 @@ def vector_embedding():
 prompt1=st.text_input("Enter your question here.....")
 
 
-if st.button("Documents Embedding"):
+if st.button("Submit"):
     vector_embedding()
 
 import time
@@ -65,7 +70,7 @@ if prompt1:
     retrieval_chain=create_retrieval_chain(retriever,document_chain)
     start=time.process_time()
     response=retrieval_chain.invoke({'input':prompt1})
-    print("Response time :",time.process_time()-start)
+    st.write("Response time :",time.process_time()-start)
     st.write(response['answer'])
 
     # With a streamlit expander
