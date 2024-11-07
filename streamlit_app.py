eb4201e15fad4a064e133f1978f5524e2c78c837
Soujatya Das <soujatya548@gmail.com>
2024-11-08 01:19:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,6 +85,7 @@ def create_prompt(input_text):
         f"""
         Answer the questions based on the provided context only.
         Please provide the most accurate response based on the question.
+        While asked to compare between two documents, please intelligently understand the items in both the documents and state the differences and similarities as mentioned in the input query
         Previous Context: {st.session_state.last_context}
         Previous Interactions:\n{previous_interactions}
         <context>
